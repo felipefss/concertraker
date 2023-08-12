ad859450efb0c7cb51c26b19d5044200cbce9edc
Felipe Santos <felipefss@gmail.com>
2023-08-12 16:26:43 +0100

    feat: change home page menu icon

--- src/app/(main)/home/components/HeaderRight/header-right.tsx
+++ src/app/(main)/home/components/HeaderRight/header-right.tsx
@@ -1,15 +1,20 @@
+import { router } from 'expo-router';
 import { TouchableOpacity } from 'react-native';
 import { IconButton } from 'react-native-paper';
 
 export function HeaderRight() {
+  function handlePressAddButton() {
+    router.push('/concert-form/add');
+  }
+
   return (
     <>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={handlePressAddButton}>
         <IconButton icon="plus" iconColor="white" />
       </TouchableOpacity>
 
       <TouchableOpacity>
-        <IconButton icon="dots-vertical" iconColor="white" />
+        <IconButton icon="menu" iconColor="white" />
       </TouchableOpacity>
     </>
   );
