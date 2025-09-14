c43a34f25e7d6ea5194aac2c451b8059ed997f9f
Felipe Santos <felipefss@gmail.com>
2025-09-14 16:12:34 +0100

    config drizzle-neon integration

--- packages/api/src/db/index.ts
+++ packages/api/src/db/index.ts
@@ -1,1 +1,7 @@
-import {} from 'drizzle-orm';
+import { drizzle } from 'drizzle-orm/neon-http';
+
+if (!Bun.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set');
+}
+
+const db = drizzle(Bun.env.DATABASE_URL);
