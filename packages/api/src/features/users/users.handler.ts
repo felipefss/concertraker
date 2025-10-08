fd9434c3d510b5482181ac650d1008cd0d3ef638
Felipe Santos <felipefss@gmail.com>
2025-10-08 22:31:53 +0100

    use something like DI with user handler

--- packages/api/src/features/users/users.handler.ts
+++ packages/api/src/features/users/users.handler.ts
@@ -2,12 +2,24 @@ import { createFactory } from 'hono/factory';
 import type { UserData } from './repository/users.repository';
 import { UsersDrizzleRepository } from './repository/users.repository.drizzle';
 
-const factory = createFactory();
+type Env = {
+  Variables: {
+    repository: UsersDrizzleRepository;
+  };
+};
 
-export const createUser = factory.createHandlers(async (c) => {
+const factory = createFactory<Env>();
+
+const useRepository = factory.createMiddleware(async (c, next) => {
+  c.set('repository', new UsersDrizzleRepository());
+  await next();
+});
+
+export const createUser = factory.createHandlers(useRepository, async (c) => {
   const { data } = await c.req.json<{ data: UserData }>();
 
-  const usersRepository = new UsersDrizzleRepository();
+  // const usersRepository = new UsersDrizzleRepository();
+  const usersRepository = c.get('repository');
   const insertedId = await usersRepository.create(data);
 
   c.status(insertedId === -1 ? 500 : 201);
