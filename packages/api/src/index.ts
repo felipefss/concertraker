b48bd98f3cbb9466248e3a7d517592a5ba9f4e47
Felipe Santos <felipefss@gmail.com>
2025-12-16 21:08:40 +0000

    API: setup express server base

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,14 +1,25 @@
-import { Hono } from 'hono';
-import { logger } from 'hono/logger';
-import { handle } from 'hono/vercel';
-import concerts from './features/concerts';
-import users from './features/users';
-import { timedLog } from './middlewares/logger';
+// import { Hono } from 'hono';
+// import { logger } from 'hono/logger';
+// import { handle } from 'hono/vercel';
+// import concerts from './features/concerts';
+// import users from './features/users';
+// import { timedLog } from './middlewares/logger';
 
-const app = new Hono().basePath('/api');
-app.use(logger(timedLog));
+// const app = new Hono().basePath('/api');
+// app.use(logger(timedLog));
 
-app.route('/users', users);
-app.route('/concerts', concerts);
+// app.route('/users', users);
+// app.route('/concerts', concerts);
 
-export default handle(app);
+// export default handle(app);
+import express from 'express';
+import morgan from './middlewares/morgan';
+
+const app = express();
+const port = 3000;
+
+app.use(morgan);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- packages/api/src/middlewares/logger.ts
+++ packages/api/src/middlewares/logger.ts
@@ -1,14 +1,11 @@
-// TODO: Use an actual more powerful logger
-export const timedLog = (message: string, ...rest: string[]) => {
-  const now = new Date();
-  const dateTimeFormat = new Intl.DateTimeFormat('en-US', {
-    day: '2-digit',
-    hour: '2-digit',
-    hour12: false,
-    minute: '2-digit',
-    month: '2-digit',
-    second: '2-digit',
-    year: 'numeric',
-  });
-  console.log(`[${dateTimeFormat.format(now)}]`, message, ...rest);
-};
+import winston from 'winston';
+
+const { combine, timestamp, json, colorize } = winston.format;
+
+const logger = winston.createLogger({
+  format: combine(colorize({ all: true }), timestamp(), json()),
+  level: 'http',
+  transports: [new winston.transports.Console()],
+});
+
+export default logger;
